ef04799f80735a79577b087654dd0db38c765e30
Alice Di Nunno <alice@dinunno.fr>
2022-01-30 14:59:46 +0100

    Initial Commit

---
+++ Shared/RackController_ClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  RackController_ClientApp.swift
+//  Shared
+//
+//  Created by Alice Di Nunno on 30/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct RackController_ClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
