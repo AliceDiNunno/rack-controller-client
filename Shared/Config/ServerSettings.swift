3f7c8159d38093bb4358f1e1a3106e35be836ca8
Alice Di Nunno <alice@dinunno.fr>
2022-02-02 20:10:30 +0100

    [Feat] Add Save on Server Settings

--- Shared/Config/ServerSettings.swift
+++ Shared/Config/ServerSettings.swift
@@ -19,16 +19,14 @@ class ServerSettings: Settings {
     private var hostKey = "host"
     @Published var host: String {
         didSet {
-            print("SET HOST")
-            set(host, forKey: hostKey)
+            save()
         }
     }
     
     private var portKey = "port"
     @Published var port: Int {
         didSet {
-            print("SET PORT")
-            set(port, forKey: portKey)
+            save()
         }
     }
     
@@ -36,36 +34,42 @@ class ServerSettings: Settings {
     private var userKey = "user"
     @Published var user: String {
         didSet {
-            print("SET USER")
-            set(user, forKey: "user")
+            save()
         }
     }
     
     private var passwordKey = "password"
     @Published var password: String {
         didSet {
-            print("SET PASS")
-            set(password, forKey: passwordKey)
+            save()
         }
     }
     
     private var hasServerInformationsKey = "hasServerInformations"
     @Published var hasServerInformations: Bool {
         didSet {
-            set(hasServerInformations, forKey: hasServerInformationsKey)
+            save()
         }
     }
     
     override init() {
         
         self.host = UserDefaults.standard.value(forKey: hostKey) as? String ?? ""
-        self.port = UserDefaults.standard.integer(forKey: portKey) ?? 30000
+        self.port = UserDefaults.standard.integer(forKey: portKey)
         
         self.user = UserDefaults.standard.value(forKey: userKey) as? String ?? ""
         self.password = UserDefaults.standard.value(forKey: passwordKey) as? String ?? ""
         
         self.hasServerInformations = UserDefaults.standard.object(forKey: hasServerInformationsKey) as? Bool ?? false
-
+        
         super.init()
+    }
+    
+    func save() {
+        set(hasServerInformations, forKey: hasServerInformationsKey)
+        set(host, forKey: hostKey)
+        set(port, forKey: portKey)
+        set(user, forKey: userKey)
+        set(password, forKey: passwordKey)
     }
 }
